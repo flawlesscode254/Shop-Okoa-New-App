02c49457c5f0a45230c14d986cdcdf13cbaa47d9
flawlesscode254 <denzme414@gmail.com>
2023-07-17 11:24:02 +0300

    Added categories data

---
+++ data/Categories.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 1,
+    title: "Clothing",
+    image: require("../assets/clothes.png"),
+  },
+  {
+    id: 2,
+    title: "Electronics",
+    image: require("../assets/electronics.png"),
+  },
+  {
+    id: 3,
+    title: "General Stores",
+    image: require("../assets/general.png"),
+  },
+  {
+    id: 4,
+    title: "Phones & Tablets",
+    image: require("../assets/phones.png"),
+  },
+  {
+    id: 5,
+    title: "Foods & Drinks",
+    image: require("../assets/foods.png"),
+  },
+  {
+    id: 6,
+    title: "Wholesale",
+    image: require("../assets/wholesaler.png"),
+  },
+];
